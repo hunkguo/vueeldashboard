483545978b6428e2d3c7556fa69fad735161c3b3
郭尧 <hunkguo@guoyaodeMBP.lan>
2018-10-31 21:07:21 +0800

    根据demo测试可用。

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
